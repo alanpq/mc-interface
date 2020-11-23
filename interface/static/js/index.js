5d01dc873658e9604a974d00dd7e2c1859a83974
Alan Panayotov <alanpanayotov@gmail.com>
2020-11-23 17:12:03 +0000

    add html escaping to clientside

--- interface/static/js/index.js
+++ interface/static/js/index.js
@@ -1,93 +1,101 @@
 'use strict'
 // TODO: fix all this dom accessing bullshit
 // window.onload = () => {
-    let conn = null
 
-    const log = (msg) => {
-        div_log.innerHTML += msg + '<br>'
-        div_log.scroll(0, div_log.scrollTop + 1000)
-    }
+const escapeEl = document.createElement("p");
 
-    const connect = () => {
-        disconnect()
+const clean = (str) => {
+    escapeEl.innerText = str;
+    return escapeEl.innerHTML;
+}
 
-        const wsUri =
-            (window.location.protocol === 'https:' ? 'wss://' : 'ws://') +
-            window.location.host +
-            '/ws/'
+let conn = null
 
-        conn = new WebSocket(wsUri)
-        log('Connecting...')
+const log = (msg) => {
+    div_log.innerHTML += msg + '<br>'
+    div_log.scroll(0, div_log.scrollTop + 1000)
+}
 
-        conn.onopen = function () {
-            log('Connected.')
-            update_ui()
-        }
+const connect = () => {
+    disconnect()
 
-        conn.onmessage = function (e) {
-            log(`<span class="">${DOMPurify.sanitize(e.data)}</span>`)
-        }
+    const wsUri =
+        (window.location.protocol === 'https:' ? 'wss://' : 'ws://') +
+        window.location.host +
+        '/ws/'
 
-        conn.onclose = function () {
-            log('Disconnected.')
-            conn = null
+    conn = new WebSocket(wsUri)
+    log('Connecting...')
 
-            update_ui()
-        }
-    }
-
-    const disconnect = () => {
-        if (conn) {
-            log('Disconnecting...')
-            conn.close()
-            conn = null
-
-            update_ui()
-        }
-    }
-
-    const update_ui = () => {
-        if (!conn) {
-            span_status.textContent = 'disconnected'
-            btn_connect.textContent = 'Connect'
-        } else {
-            span_status.textContent = `connected (${conn.protocol})`
-            btn_connect.textContent = 'Disconnect'
-        }
-    }
-    
-    const cmd = (n) => {
-        if (!conn) return
-        conn.send("scmd" + n)
-    }
-
-    btn_connect.onclick = () => {
-        if (!conn) {
-            connect()
-        } else {
-            disconnect()
-        }
-
+    conn.onopen = function () {
+        log('Connected.')
         update_ui()
     }
 
-    btn_send.onclick = () => {
-        if (!conn) return
-
-        const text = input_text.value
-        log(`<span class="green">&gt; ${DOMPurify.sanitize(text)}</span>`)
-        conn.send("rcon" + text)
-
-        input_text.value = ''
-        input_text.focus()
+    conn.onmessage = function (e) {
+        log(`<span class="">${clean(e.data)}</span>`)
     }
 
-    input_text.onkeyup = (e) => {
-        if (e.key === 'Enter') {
-            btn_send.click()
-        }
+    conn.onclose = function () {
+        log('Disconnected.')
+        conn = null
+
+        update_ui()
+    }
+}
+
+const disconnect = () => {
+    if (conn) {
+        log('Disconnecting...')
+        conn.close()
+        conn = null
+
+        update_ui()
+    }
+}
+
+const update_ui = () => {
+    if (!conn) {
+        span_status.textContent = 'disconnected'
+        btn_connect.textContent = 'Connect'
+    } else {
+        span_status.textContent = `connected (${conn.protocol})`
+        btn_connect.textContent = 'Disconnect'
+    }
+}
+
+const cmd = (n) => {
+    if (!conn) return
+    conn.send("scmd" + n)
+}
+
+btn_connect.onclick = () => {
+    if (!conn) {
+        connect()
+    } else {
+        disconnect()
     }
 
-    document.getElementById("btn_start").onclick = () => {cmd(0)}
-    document.getElementById("btn_stop").onclick = () => {cmd(1)}
+    update_ui()
+}
+
+btn_send.onclick = () => {
+    if (!conn) return
+
+    const text = input_text.value
+    log(`<span class="green">&gt; ${clean(text)}</span>`)
+    conn.send("rcon" + text)
+
+    input_text.value = ''
+    input_text.focus()
+}
+
+input_text.onkeyup = (e) => {
+    if (e.key === 'Enter') {
+        btn_send.click()
+    }
+}
+
+document.getElementById("btn_start").onclick = () => { cmd(0) }
+document.getElementById("btn_stop").onclick = () => { cmd(1) }
 // }
